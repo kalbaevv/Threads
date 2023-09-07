580f39e0f086e892a5d69d4d750b769629ea77bb
iskender <even10iq@gmail.com>
2023-09-07 05:20:35 +0000

    fixed rightsidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function RightSidebar() {
-  return <section className="custom-scroll-bar rightsidebar"></section>;
+  return (
+    <section className="custom-scroll-bar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested communities
+        </h3>
+      </div>
+
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">Suggested users</h3>
+      </div>
+    </section>
+  );
 }
